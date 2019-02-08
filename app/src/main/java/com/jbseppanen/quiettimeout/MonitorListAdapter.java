2b83011095f9d1e86d2fb8e9cc17cf2284b19155
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-08 11:09:48 -0800

    Update card layout in main recycler view to use a progress bar to show threshold.

--- app/src/main/java/com/jbseppanen/quiettimeout/MonitorListAdapter.java
+++ app/src/main/java/com/jbseppanen/quiettimeout/MonitorListAdapter.java
@@ -8,13 +8,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MonitorListAdapter extends RecyclerView.Adapter<MonitorListAdapter.ViewHolder> {
     static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView monitorViewDuration, monitorViewThreshold;
+        TextView monitorViewDuration;
+        ProgressBar progressBar;
         ViewGroup parentView;
         int lastPosition;
 
@@ -23,8 +25,8 @@ public class MonitorListAdapter extends RecyclerView.Adapter<MonitorListAdapter.
 
             lastPosition = -1;
             monitorViewDuration = itemView.findViewById(R.id.element_duration);
-            monitorViewThreshold = itemView.findViewById(R.id.element_threshold);
-            parentView = itemView.findViewById(R.id.monitor_element_parent_layout);
+            progressBar = itemView.findViewById(R.id.progress_element_threshold);
+                    parentView = itemView.findViewById(R.id.monitor_element_parent_layout);
         }
     }
 
@@ -59,8 +61,8 @@ public class MonitorListAdapter extends RecyclerView.Adapter<MonitorListAdapter.
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
         final Monitor data = dataList.get(i);
 
-        viewHolder.monitorViewDuration.setText(String.format("%02d:%02d:%02d",(int) data.getDuration() / 3600000, (int) (( data.getDuration() % 3600000)/60000), (int) (( data.getDuration() % 60000) / 1000)));
-        viewHolder.monitorViewThreshold.setText(String.valueOf(data.getThreshold()));
+        viewHolder.monitorViewDuration.setText(String.format("%02d:%02d:%02d", (int) data.getDuration() / 3600000, (int) ((data.getDuration() % 3600000) / 60000), (int) ((data.getDuration() % 60000) / 1000)));
+        viewHolder.progressBar.setProgress(data.getThreshold());
         viewHolder.parentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -74,7 +76,7 @@ public class MonitorListAdapter extends RecyclerView.Adapter<MonitorListAdapter.
             @Override
             public boolean onLongClick(View v) {
                 Intent intent = new Intent(context, EditMonitorActivity.class);
-                intent.putExtra(EditMonitorActivity.EDIT_MONITOR_KEY,data);
+                intent.putExtra(EditMonitorActivity.EDIT_MONITOR_KEY, data);
                 activity.startActivity(intent);
                 return true;
             }
